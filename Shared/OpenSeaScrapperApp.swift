aa643902c94e77b5003cd13e40393e3282f151a5
Pietro Santececca <pietrosantececca@gmail.com>
2022-04-10 19:24:10 +0200

    Initial Commit

---
+++ Shared/OpenSeaScrapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenSeaScrapperApp.swift
+//  Shared
+//
+//  Created by Pietro on 10/04/22.
+//
+
+import SwiftUI
+
+@main
+struct OpenSeaScrapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
